96443f141576dd90bd371ff3f06afb16132594cb
VEM-252 <np64574475@gmail.com>
2025-12-26 10:59:52 +0530

    NEO ⚡

--- VIPMUSIC/core/bot.py
+++ VIPMUSIC/core/bot.py
@@ -11,7 +11,7 @@ class VIPBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting bot...")
         super().__init__(
-            name="ShrutiMusic",
+            name="VIPMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
