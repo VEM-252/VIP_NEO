15ae65475144dcc40e659540a5056e656c191aca
VEM-252 <np64574475@gmail.com>
2025-12-26 09:15:05 +0530

    Refactor init function and improve logging

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -1,14 +1,5 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
 import asyncio
 import importlib
-
 from pyrogram import idle
 
 import config
@@ -18,8 +9,8 @@ from VIPMUSIC.core.call import VIP
 from VIPMUSIC.plugins import ALL_MODULES
 from VIPMUSIC.utils.database import get_banned_users, get_gbanned
 
-
 async def init():
+    # Assistant Clients Check
     if (
         not config.STRING1
         and not config.STRING2
@@ -27,15 +18,16 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("VIPMUSIC").error(
-            "No Assistant Clients Vars Defined!.. Exiting Process."
-        )
+        LOGGER("VIPMUSIC").error("No Assistant Clients Vars Defined!.. Exiting Process.")
         return
+
+    # Spotify Check
     if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
         LOGGER("VIPMUSIC").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
 
+    # Database loading (Error handling without logging - as you requested)
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -46,23 +38,34 @@ async def init():
     except Exception:
         pass
 
+    # Bot Start
     await app.start()
 
+    # Module Loading
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(all_module)
+        try:
+            imported_module = importlib.import_module(all_module)
+            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+                if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                    HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+        except Exception:
+            pass
 
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules ")
+    LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules")
 
+    # Final Services Startup
     await userbot.start()
     await VIP.start()
     await VIP.decorators()
+    
     LOGGER("VIPMUSIC").info("VIPMUSIC STARTED SUCCESSFULLY 🕊️")
+    
     await idle()
-
+    
+    # Graceful Shutdown
+    await app.stop()
+    await userbot.stop()
+    LOGGER("VIPMUSIC").info("Stopping VIPMUSIC! GoodBye")
 
 if __name__ == "__main__":
-    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
-    LOGGER("VIPMUSIC").info("Stopping VIPMUSIC! GoodBye")
+    asyncio.get_event_loop().run_until_complete(init())
